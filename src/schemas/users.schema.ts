862d00764c0b825ca00e09826c76dfcf5d1841e0
Vadzim Alkhimionak <v.alkhimyonok@revoteam.pro>
2025-04-06 20:47:49 +0300

    update users schema

--- src/schemas/users.schema.ts
+++ src/schemas/users.schema.ts
@@ -38,6 +38,41 @@ export const GetUsersByIdsInputSchema = z.object({
   usersIds: z.array(z.string()),
 });
 
+export const CreateUserInputSchema = z.object({
+  userInfo: UserInfoSchema,
+});
+
+export const DeleteUserInputSchema = z.object({
+  docId: z.string(),
+  uid: z.string(),
+});
+
+export const GetUserInputSchema = z.object({
+  id: z.string(),
+});
+
+export const UpdateUserInputSchema = z.object({
+  docId: z.string(),
+  data: z.record(z.any()),
+});
+
+export const DefaultSuccessResponseSchema = z.object({
+  success: z.literal(true),
+});
+
+export const GetUserOutputSchema = UserInfoSchema.nullable();
+
+export const GetUsersByIdsOutputSchema = z.array(UserInfoSchema);
+
 export type IUserInfo = z.infer<typeof UserInfoSchema>;
 export type GetUsersByIdsInput = z.infer<typeof GetUsersByIdsInputSchema>;
 export type UserStatusType = z.infer<typeof UserStatusTypeEnum>;
+export type CreateUserInput = z.infer<typeof CreateUserInputSchema>;
+export type DeleteUserInput = z.infer<typeof DeleteUserInputSchema>;
+export type GetUserInput = z.infer<typeof GetUserInputSchema>;
+export type UpdateUserInput = z.infer<typeof UpdateUserInputSchema>;
+export type DefaultSuccessResponse = z.infer<
+  typeof DefaultSuccessResponseSchema
+>;
+export type GetUserOutput = z.infer<typeof GetUserOutputSchema>;
+export type GetUsersByIdsOutput = z.infer<typeof GetUsersByIdsOutputSchema>;
